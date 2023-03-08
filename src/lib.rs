a967e892d1bfdc14bf0bfd9d7642146363893fde
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-03-07 22:51:03 -0500

    Implement Matrix::random() to get a new matrix filled with random data.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+use rand::prelude::Distribution;
+use rand::distributions::Standard;
+
 struct Matrix<T> {
     num_rows: usize,
     num_cols: usize,
@@ -28,6 +32,39 @@ impl<T: Default + Copy> Matrix<T> {
     }
 }
 
+impl<T: Default + Copy> Matrix<T> {
+    pub fn random(num_rows: usize, num_cols: usize) -> Result<Matrix<T>, String>
+    where
+        Standard: Distribution<T>,
+    {
+        let mut matrix = Self::new(num_rows, num_cols)?;
+
+        let mut rng = rand::thread_rng();
+
+        for i in 0..num_rows {
+            for j in 0..num_cols {
+                matrix[i][j] = rng.gen();
+            }
+        }
+
+        Ok(matrix)
+    }
+}
+
+impl<T> std::ops::Index<usize> for Matrix<T> {
+    type Output = Vec<T>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index]
+    }
+}
+
+impl<T> std::ops::IndexMut<usize> for Matrix<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Vec<T> {
+        &mut self.data[index]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,9 +75,31 @@ mod tests {
 
         assert_eq!(matrix1.num_rows, 2);
         assert_eq!(matrix1.num_cols, 3);
-        assert_eq!(matrix1.data, vec![vec![0,0,0], vec![0,0,0]]);
+        assert_eq!(matrix1.data, vec![vec![0, 0, 0], vec![0, 0, 0]]);
         assert_eq!(matrix1.is_square(), false);
 
-        assert!(Matrix::<usize>::new(0,0).is_err());
+        assert!(Matrix::<usize>::new(0, 0).is_err());
+    }
+
+    #[test]
+    fn is_square() {
+        assert!(Matrix::<usize>::new(3, 3).unwrap().is_square());
+        assert!(!Matrix::<usize>::new(3, 2).unwrap().is_square());
+    }
+
+    #[test]
+    fn random_returns_errors() {
+        assert!(Matrix::<usize>::random(0, 0).is_err());
+        assert!(Matrix::<usize>::random(1, 1).is_ok());
+    }
+
+    #[test]
+    fn random_is_filled() {
+        let matrix1: Matrix<f32> = Matrix::random(3, 3).unwrap();
+        let matrix2: Matrix<usize> = Matrix::random(3, 3).unwrap();
+        let matrix3: Matrix<i8> = Matrix::random(3, 3).unwrap();
+        assert!(matrix1.data.iter().flatten().any(|&x| x != 0.0));
+        assert!(matrix2.data.iter().flatten().any(|&x| x != 0));
+        assert!(matrix3.data.iter().flatten().any(|&x| x != 0));
     }
 }
