0263bd2be610b5ba94803dfa4e544b0321891cc8
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-03-11 14:06:21 -0500

    Simplify library scope for now.

--- src/lib.rs
+++ src/lib.rs
@@ -37,77 +37,21 @@ struct Matrix<T, Order> {
 }
 
 impl<T: Default + Copy, O: Order> Matrix<T, O> {
-    #[inline]
-    pub fn from_blind_fn<F: FnMut() -> T>(
-        num_rows: usize,
-        num_cols: usize,
-        f: F,
-    ) -> Result<Self, String> {
-        if num_rows == 0 {
-            return Err("Number of rows cannot be less than or equal to 0.".to_string());
-        };
+    pub fn new(num_rows: usize, num_cols: usize) -> Result<Self, String> {
+        if num_rows * num_cols == 0 {
+            return Err("Number of rows or number of columns cannot be 0.".to_string());
+        }
 
-        if num_cols == 0 {
-            return Err("Number of columns cannot be less than or equal to 0.".to_string());
-        };
-
-        let data = std::iter::repeat_with(f)
-            .take(num_rows * num_cols)
-            .collect();
+        let data = vec![T::default(); num_rows * num_cols];
 
         Ok(Self {
             num_rows,
             num_cols,
             data,
-            _order: PhantomData,
+            _order: PhantomData
         })
     }
-
-    pub fn default(num_rows: usize, num_cols: usize) -> Result<Self, String>
-        where
-            T: Default,
-    {
-        Self::from_blind_fn(num_rows, num_cols, || T::default())
-    }
-
-    pub fn random(num_rows: usize, num_cols: usize) -> Result<Self, String>
-        where
-            Standard: Distribution<T>,
-    {
-        let mut rng = rand::thread_rng();
-        Self::from_blind_fn(num_rows, num_cols, || rng.gen())
-    }
-
-    pub fn square(size: usize) -> Result<Self, String> {
-        Self::default(size, size)
-    }
-
-    pub fn square_random(size: usize) -> Result<Self, String>
-        where
-            Standard: Distribution<T>,
-    {
-        Self::random(size, size)
-    }
-
-    pub fn from_elem(num_rows: usize, num_cols: usize, elem: T) -> Result<Self, String>
-        where
-            T: Clone,
-    {
-        Self::from_blind_fn(num_rows, num_cols, || elem)
-    }
-
-    pub fn from_vec(num_rows: usize, num_cols: usize, vec: Vec<T>) -> Result<Self, String>
-    {
-        if vec.len() != num_rows * num_cols {
-            return Err("Input vector length does not match matrix dimensions.".to_string());
-        }
-
-        let mut cloned_vec = vec;
-
-        Self::from_blind_fn(num_rows, num_cols, || cloned_vec.remove(0))
-    }
-
-    pub fn make_eye(&mut self) -> Result<(), String>
+    pub fn set_identity(&mut self) -> Result<(), String>
         where
             T: num_traits::One,
     {
@@ -124,6 +68,10 @@ impl<T: Default + Copy, O: Order> Matrix<T, O> {
 
     pub fn is_square(&self) -> bool {
         self.num_rows == self.num_cols
+    }
+
+    pub fn transpose(&self) -> Result<Self, String> {
+        std::unimplemented!()
     }
 }
 
@@ -149,66 +97,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_blind_fn() {
-        let m1: Matrix<usize, ColMajor> = Matrix::from_blind_fn(2, 3, || 0).unwrap();
-
-        assert_eq!(m1[(0, 0)], 0);
-        assert_eq!(m1[(0, 1)], 0);
-        assert_eq!(m1[(1, 0)], 0);
-        assert_eq!(m1[(1, 1)], 0);
-
-        let m1: Matrix<usize, ColMajor> = Matrix::from_blind_fn(3, 2, || 2).unwrap();
-
-        assert_eq!(m1[(0, 0)], 2);
-        assert_eq!(m1[(0, 1)], 2);
-        assert_eq!(m1[(1, 0)], 2);
-        assert_eq!(m1[(1, 1)], 2);
-        assert_eq!(m1[(2, 0)], 2);
-        assert_eq!(m1[(2, 1)], 2);
-
-        let m1 = Matrix::<usize, RowMajor>::from_blind_fn(0, 3, || 2);
-
-        assert!(m1.is_err());
-    }
-
-    #[test]
-    fn from_elem() {
-        let m1: Matrix<usize, ColMajor> = Matrix::from_elem(3, 3, 5).unwrap();
-
-        for i in 0..m1.num_rows {
-            for j in 0..m1.num_cols {
-                assert_eq!(m1[(i, j)], 5);
-            }
-        }
-    }
-
-    #[test]
-    fn test_from_vec() {
-        let input_vec = vec![1, 2, 3, 4, 5, 6];
-
-        let m1: Matrix<usize, ColMajor> = Matrix::from_vec(2, 3, input_vec).unwrap();
-
-        assert_eq!(m1[(0, 0)], 1);
-        assert_eq!(m1[(0, 1)], 2);
-        assert_eq!(m1[(0, 2)], 3);
-        assert_eq!(m1[(1, 0)], 4);
-        assert_eq!(m1[(1, 1)], 5);
-        assert_eq!(m1[(1, 2)], 6);
-
-        let input_vec: Vec<usize> = vec![1, 2, 3, 4, 5];
-
-        let m1: Result<Matrix<usize, ColMajor>, String> = Matrix::from_vec(2, 3, input_vec);
-
-        assert!(m1.is_err());
-        assert_eq!(
-            m1.err().unwrap(),
-            "Input vector length does not match matrix dimensions."
-        );
-    }
-
-    #[test]
     fn make_matrix() {
-        let m1: Matrix<usize, ColMajor> = Matrix::default(2, 3).unwrap();
+        let m1: Matrix<usize, ColMajor> = Matrix::new(2, 3).unwrap();
 
         assert_eq!(m1.num_rows, 2);
         assert_eq!(m1.num_cols, 3);
@@ -216,49 +106,6 @@ mod tests {
         assert_eq!(m1.data.len(), 6);
         assert!(!m1.is_square());
 
-        assert!(Matrix::<usize, ColMajor>::default(0, 0).is_err());
-    }
-
-    #[test]
-    fn random_is_filled() {
-        let m1: Matrix<f32, ColMajor> = Matrix::random(3, 3).unwrap();
-        let matrix2: Matrix<usize, ColMajor> = Matrix::random(3, 3).unwrap();
-        let matrix3: Matrix<i8, ColMajor> = Matrix::random(3, 3).unwrap();
-        assert!(m1.data.iter().any(|&x| x != 0.0));
-        assert!(matrix2.data.iter().any(|&x| x != 0));
-        assert!(matrix3.data.iter().any(|&x| x != 0));
-
-        let m1: Matrix<f32, ColMajor> = Matrix::square_random(3).unwrap();
-        let matrix2: Matrix<usize, ColMajor> = Matrix::square_random(3).unwrap();
-        let matrix3: Matrix<i8, ColMajor> = Matrix::square_random(3).unwrap();
-        assert!(m1.data.iter().any(|&x| x != 0.0));
-        assert!(matrix2.data.iter().any(|&x| x != 0));
-        assert!(matrix3.data.iter().any(|&x| x != 0));
-    }
-
-    #[test]
-    fn square() {
-        let m1: Matrix<usize, ColMajor> = Matrix::square(3).unwrap();
-        assert!(m1.is_square());
-
-        let m2: Result<Matrix<usize, ColMajor>, String> = Matrix::square(0);
-        assert!(m2.is_err());
-
-        let m2: Result<Matrix<usize, ColMajor>, String> = Matrix::square(10);
-        assert!(m2.is_ok());
-    }
-
-    #[test]
-    fn square_random() {
-        let m1: Matrix<usize, ColMajor> = Matrix::square_random(3).unwrap();
-        assert!(m1.is_square());
-        assert_eq!(m1.num_rows, 3);
-        assert_eq!(m1.num_cols, 3);
-
-        let matrix2: Result<Matrix<usize, ColMajor>, String> = Matrix::square_random(0);
-        assert!(matrix2.is_err());
-
-        let matrix2: Result<Matrix<usize, ColMajor>, String> = Matrix::square_random(10);
-        assert!(matrix2.is_ok());
+        assert!(Matrix::<usize, ColMajor>::new(0, 0).is_err());
     }
 }
