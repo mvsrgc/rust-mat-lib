a88f340dc2eb8048413048e73c32d28ff6fcce93
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-03-11 21:59:57 -0500

    Read num. of row and cols from file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use rand::distributions::Standard;
-use rand::prelude::Distribution;
-use rand::Rng;
+use std::fmt::Debug;
 use std::marker::PhantomData;
-use std::cmp;
-use num_traits;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str::FromStr;
+use num_traits::one;
 
 pub trait Order {
     fn calc_index(pos: (usize, usize), dims: (usize, usize)) -> usize;
@@ -29,6 +29,11 @@ impl Order for ColMajor {
     }
 }
 
+struct Dimensions {
+    rows: usize,
+    cols: usize,
+}
+
 struct Matrix<T, Order> {
     num_rows: usize,
     num_cols: usize,
@@ -36,7 +41,7 @@ struct Matrix<T, Order> {
     _order: PhantomData<Order>,
 }
 
-impl<T: Default + Copy, O: Order> Matrix<T, O> {
+impl<T: Default + Copy + FromStr + Debug, O: Order> Matrix<T, O> {
     pub fn new(num_rows: usize, num_cols: usize) -> Result<Self, String> {
         if num_rows * num_cols == 0 {
             return Err("Number of rows or number of columns cannot be 0.".to_string());
@@ -48,7 +53,7 @@ impl<T: Default + Copy, O: Order> Matrix<T, O> {
             num_rows,
             num_cols,
             data,
-            _order: PhantomData
+            _order: PhantomData,
         })
     }
     pub fn set_identity(&mut self) -> Result<(), String>
@@ -73,6 +78,38 @@ impl<T: Default + Copy, O: Order> Matrix<T, O> {
     pub fn transpose(&self) -> Result<Self, String> {
         std::unimplemented!()
     }
+
+    pub fn dims(&self) -> Dimensions {
+        Dimensions { rows: self.num_rows, cols: self.num_cols }
+    }
+
+    pub fn from_file(file: &mut File) -> Result<Self, String> {
+        let mut reader = BufReader::new(file);
+
+        let mut num_rows = 0;
+        let mut num_cols = 0;
+        for (i, line) in reader.lines().enumerate() {
+            let line = line.unwrap_or_default();
+            let values: Vec<&str> = line.split_whitespace().collect();
+            if i == 0 {
+                match (values.get(0), values.get(1)) {
+                    (Some(rows), Some(cols)) => {
+                        num_rows = usize::from_str(rows).unwrap_or_default();
+                        num_cols = usize::from_str(cols).unwrap_or_default();
+                        continue;
+                    }
+                    _ => return Err("File is empty or has invalid format.".to_string()),
+                }
+            }
+        }
+
+        Ok(Self {
+            num_rows,
+            num_cols,
+            data: vec![T::default(); 1],
+            _order: PhantomData,
+        })
+    }
 }
 
 impl<T, O: Order> std::ops::Index<(usize, usize)> for Matrix<T, O> {
@@ -94,6 +131,7 @@ impl<T, O: Order> std::ops::IndexMut<(usize, usize)> for Matrix<T, O> {
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
     use super::*;
 
     #[test]
@@ -107,5 +145,43 @@ mod tests {
         assert!(!m1.is_square());
 
         assert!(Matrix::<usize, ColMajor>::new(0, 0).is_err());
+    }
+
+    #[test]
+    fn identity() {
+        let mut m1: Matrix<usize, ColMajor> = Matrix::new(2, 2).unwrap();
+        m1.set_identity().unwrap();
+        assert_eq!(m1[(0, 1)], usize::default());
+        assert_eq!(m1[(1, 0)], usize::default());
+        assert_eq!(m1[(0, 0)], 1);
+        assert_eq!(m1[(1, 1)], 1);
+        assert_eq!(m1.dims().rows, 2);
+        assert_eq!(m1.dims().cols, 2);
+
+        let mut m1: Matrix<usize, ColMajor> = Matrix::new(3, 2).unwrap();
+        assert!(m1.set_identity().is_err());
+
+        let mut m1: Matrix<usize, ColMajor> = Matrix::new(2, 2).unwrap();
+        assert!(m1.set_identity().is_ok())
+    }
+
+    #[test]
+    fn is_square() {
+        let m1: Matrix<usize, ColMajor> = Matrix::new(3, 3).unwrap();
+        assert!(m1.is_square());
+
+        let m1: Matrix<usize, ColMajor> = Matrix::new(3, 2).unwrap();
+        assert!(!m1.is_square());
+    }
+
+    #[test]
+    fn from_file() {
+        let path = PathBuf::from("data/input.txt");
+        let mut file = File::open(path).unwrap();
+
+        let result: Matrix<f64, ColMajor> = Matrix::from_file(&mut file).unwrap();
+
+        assert_eq!(result.dims().rows, 4);
+        assert_eq!(result.dims().cols, 5);
     }
 }
